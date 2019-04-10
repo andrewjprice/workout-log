ffd047f44fdac476a78749aa257cef31e6b6a18f
Andrew <andrew.j.price@outlook.com>
2019-04-10 16:25:28 -0600

    Update home view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'pages#index'
   devise_for :users
+
+  authenticated :user do
+    root 'authenticated#index', as: :authenticated_root
+  end
+
+  root 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
